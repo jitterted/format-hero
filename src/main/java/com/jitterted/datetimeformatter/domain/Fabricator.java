0d58a84a36f2fd29dab68cc707602240702a5e80
Ted M. Young <tedyoung@gmail.com>
2022-01-26 17:09:19 -0800

    Refactored to change from a String for the pattern to a list of fragments that are assembled when pattern() is called.

--- src/main/java/com/jitterted/datetimeformatter/domain/Fabricator.java
+++ src/main/java/com/jitterted/datetimeformatter/domain/Fabricator.java
@@ -2,24 +2,23 @@ package com.jitterted.datetimeformatter.domain;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fabricator {
-    private String pattern;
+    private final List<String> fragments = new ArrayList<>();
 
     public Fabricator with(String fragment) {
-        if (pattern == null) {
-            pattern = fragment;
-        } else {
-            pattern = pattern + "-" + fragment;
-        }
+        fragments.add(fragment);
         return this;
     }
 
     public String pattern() {
-        return pattern;
+        return String.join("-", fragments);
     }
 
     public String formatFor(ZonedDateTime zoneDateTimeUtc) {
-        return DateTimeFormatter.ofPattern(pattern).format(zoneDateTimeUtc);
+        return DateTimeFormatter.ofPattern(pattern())
+                                .format(zoneDateTimeUtc);
     }
 }
